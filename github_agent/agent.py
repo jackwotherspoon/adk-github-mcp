009f95a1890344985b5129fffb5658de5d5a8457
Jack Wotherspoon <jackwoth@google.com>
2025-06-15 18:32:57 -0400

    chore: use connection param over server param

--- github_agent/agent.py
+++ github_agent/agent.py
@@ -1,7 +1,6 @@
 import os
 from google.adk.agents import LlmAgent
-from google.adk.tools.mcp_tool import MCPToolset
-from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPServerParams
+from google.adk.tools.mcp_tool import MCPToolset, StreamableHTTPConnectionParams
 
 root_agent = LlmAgent(
     model="gemini-2.5-pro-preview-06-05",
@@ -9,7 +8,7 @@ root_agent = LlmAgent(
     instruction="You are a helpful assistant that can answer questions about GitHub.",
     tools=[
         MCPToolset(
-            connection_params=StreamableHTTPServerParams(
+            connection_params=StreamableHTTPConnectionParams(
                 url="https://api.githubcopilot.com/mcp/",
                 headers={
                     "Authorization": "Bearer " + os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN"),
